3079f3e77d7a2cc5e4d4a96335494f28ce01c296
Andrew Boyton <aboyton@users.noreply.github.com>
2020-07-10 08:42:47 +1000

    Update named-arguments.ts
    While TypeScript has implemented labeled tuples: https://devblogs.microsoft.com/typescript/announcing-typescript-4-0-beta/#labeled-tuple-elements
    
    ```ts
    function area([width, height]: [width: number, height: number]): number {
        return width * height
    }
    area([2, 3])
    ```
    
    the most idiomatic way to do named parameters in JavaScript is through simple object literals.

--- code/named-arguments.ts
+++ code/named-arguments.ts
@@ -1,1 +1,6 @@
-------
+// JavaScript uses object literals to simulate named parameters
+function area({ width, height }: { width: number, height: number }): number {
+    return width * height
+}
+area({ width: 2, height: 3 })
+
